c5520aec264f6430b231021dc2d476d27374426f
Hannes Lerchl <hannes.lerchl@googlemail.com>
2019-07-01 08:58:29 +0200

    Removed 'all same' anagrams
    This changes filters all the anagrams which are 'the same words' but
    differ in upper/lowercase (mostly the first character). It still keeps
    such anagram if there is another valid permutation of characters.
    
    Number of anagrams dropped from 30404 to 25027.

--- haskell_src/Words.hs
+++ haskell_src/Words.hs
@@ -24,6 +24,8 @@ anagrams x y = sort x == sort y
 
 add_word:: String -> HashMap String [String] -> HashMap String [String]
 list_anagrams:: HashMap String [String] -> [[String]]
+all_same :: [String] -> Bool
+equal_words :: String -> String -> Bool
 
 add_word w h =
     let w_upper = Data.List.map toUpper w
@@ -33,6 +35,16 @@ list_anagrams h =
     let ana_only = Data.HashMap.Strict.filter (\ x -> length x > 1) h
     in elems ana_only
 
+equal_words x y =
+    let x_upper = Data.List.map toUpper x
+    in x_upper == Data.List.map toUpper y
+
+all_same [] = True
+all_same [_] = True
+all_same (x:xs) =
+    let upper_x = Data.List.map toUpper x
+    in all (\ w -> equal_words upper_x (Data.List.map toUpper w)) xs
+
 all_anagrams lst =
     let word_map = Data.List.foldr (add_word) empty lst
-    in list_anagrams word_map
+    in Data.List.filter (not . all_same) (list_anagrams word_map)
